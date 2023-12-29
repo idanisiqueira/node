0ce6554d8b436f80ebd186fcc58779b67a8189d3
idanisiqueira <dcess360@gmail.com>
2023-12-29 19:52:42 -0300

    Conectando ao front React

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
-const port = 3000
+const cors = require('cors')
+const port = 3001
 
 const app = express()
 const uuid = require('uuid')
 app.use(express.json())
+app.use(cors())
 
 const users = []
 
